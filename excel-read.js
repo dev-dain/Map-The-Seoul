ca80a0ba24d6e8d9e50f1c595814485de4a8bd0e
Dain Kim <dev.dain.k@gmail.com>
2020-08-08 00:49:54 +0900

    First commit

---
+++ excel-read.js
@@ -0,0 +1,25 @@
+﻿function parseExcel() {
+  const xlsx = require('xlsx');
+  const fs = require('fs');
+  const excel = xlsx.readFile('map.xlsx');
+  const sheetName = excel.SheetNames[0];
+  const firstSheet = excel.Sheets[sheetName];
+  const jsonData = xlsx.utils.sheet_to_json(firstSheet, { defval : "" });
+
+  const latlng = jsonData.map((data, index) => {
+    return { 
+      dong: data['동명'],
+      index: index + 2,
+      roadName: data['소재지도로명주소'],
+      name: data['설치장소'],
+      lat: data['위도'], 
+      lng: data['경도']
+    }
+  });
+
+  fs.writeFile('./public/latlng.json', JSON.stringify(latlng), 'utf8', () => {
+    console.log('Done');  
+  });
+}
+
+module.exports = parseExcel;
